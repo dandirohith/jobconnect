61f5cdd666eb6c37fa8d044f6e85b4f685404e00
rohith dandi <rohith.dandi1712@gmail.com>
2024-02-22 22:22:37 +0530

    main

--- client/src/features/auth/authService.js
+++ client/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://jobconnect-api.onrender.com";
+const API_URL = "https://jobconnect-api.onrender.com/";
 
 // Register user
 const register = async (userData) => {
